d2b6df7760571e8e9bdb825237097699e59fb04c
Guilherme Rodrigo <145240089+Mannax002@users.noreply.github.com>
2025-05-08 14:26:24 -0300

    Update Linhadotempo.lua

--- Linhadotempo.lua
+++ Linhadotempo.lua
@@ -132,3 +132,116 @@ onKeyDown(function(keys)
       TpLtempo.close();
     end
 end);
+
+
+
+
+
+
+
+
+-- Lista de cidades
+local cities = {
+  'Earth',       -- FREE
+  'M2',
+  'Tsufur',
+  'Zelta',
+  'Vegeta',
+  'Namek',
+  'Gardia',
+  'Lude',
+  'Premia',
+  'City 17',
+  'Ruudese',
+  'Kanassa',
+  'Gelbo',
+  'Tritek',
+  'Rygol',
+  'CC21',        -- FREE
+  'Yardratto'
+}
+
+-- Nome do NPC
+local npcName = 'Gate Keaper'
+
+-- Variáveis de controle
+local lastTravel = 0
+local travelDelay = 3000 -- 3 segundos
+
+-- Cria a interface
+travelUI = setupUI([[  
+UIWindow
+  !text: tr('viajar')
+  color: #99d6ff
+  font: sans-bold-16px
+  size: 100 100
+  background-color: black
+  opacity: 0.85
+  anchors.left: parent.left
+  anchors.top: parent.top
+  margin-left: 600
+  margin-top: 150
+
+  ComboBox
+    id: travelOptions
+    anchors.horizontalCenter: parent.horizontalCenter
+    anchors.top: parent.top
+    text-align: center
+    opacity: 1.0
+    color: yellow
+    font: sans-bold-16px
+    margin-top: 25
+    @onSetup: |
+      self:addOption("None")
+
+  Button
+    id: closeButton
+    text: X
+    anchors.right: parent.right
+    anchors.bottom: parent.bottom
+    color: #99d6ff
+    size: 15 15
+    margin-bottom: 10
+    margin-right: 10
+]], g_ui.getRootWidget())
+
+travelUI:hide()
+
+-- Adiciona as cidades ao combo
+for _, city in ipairs(cities) do
+  travelUI.travelOptions:addOption(city)
+end
+
+-- Fecha a janela ao clicar no X
+travelUI.closeButton.onClick = function()
+  travelUI:hide()
+end
+
+-- Envia mensagem ao NPC
+NPC.talk = function(text)
+  if g_game.getClientVersion() >= 810 then
+    g_game.talkChannel(11, 0, text)
+  else
+    return say(text)
+  end
+end
+
+-- Mostra a UI se o NPC estiver por perto
+macro(100, function()
+  local findNpc = getCreatureByName(npcName)
+  if findNpc and getDistanceBetween(pos(), findNpc:getPosition()) <= 2 then
+  travelUI:show()
+  else
+  travelUI:hide()
+  end
+end)
+
+travelUI.travelOptions.onOptionChange = function(widget, option, data)
+  say('hi')
+  schedule(800, function()
+  NPC.talk(option)
+  end)
+  schedule(1600, function()
+  NPC.talk('yes')
+  end)
+end
